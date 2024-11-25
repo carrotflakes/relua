1cb52330081ec17e7ee81533a4bc6f1924ebbc5b
carrotflakes <carrotflakes@gmail.com>
2024-11-26 01:48:49 +0900

    Make  tests.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 pub mod ast;
 pub mod front;
 pub mod lua;
+#[cfg(test)]
+mod tests;
 pub mod r#type;
 pub mod type_check;
 
@@ -108,80 +110,4 @@ pub fn default_bindings() -> HashMap<String, r#type::Type> {
     .map(|(name, value)| (name.to_owned(), value))
     .collect();
     bindings
-}
-
-#[test]
-fn test() {
-    let srcs = [
-        r#"
-fn main(a: num) -> num {
-    let b: num = 1
-    return a * 2 + b
-}
-"#,
-        r#"
-fn main() {
-    let b: num = 1 + 2
-}
-"#,
-        r#"
-let a: {1, 2} = {1, 2}
-"#,
-        r#"
-let a: num | str = 1 || "a" && 2
-"#,
-        r#"
-let a: {f: (num) -> num} = {
-    f: fn(a: num) -> num {
-        return a * 2
-    }
-}"#,
-        r#"
-fn f() -> () {
-    f()
-}"#,
-        r#"
-fn f() {
-    len!{1, 2, 3}
-}"#,
-        r#"
-let t: {[table]: num, [fn]: str} = {}
-t[{1, 2}] = 3
-t[fn() -> () {}] = "a"
-"#,
-        r#"
-{1: "1", [2]: "2", ["3"]: "3"}
-"#,
-        r#"
-let f: (num) -> () = fn(a: num, b: num) -> () {}
-let f: (num, num | ()) -> () = fn(a: num) -> () {}
-"#,
-    ];
-    for src in &srcs {
-        let prog = front::parser::program(src).unwrap();
-        let res = type_check::check_program(default_bindings(), &prog);
-        gilder::assert_golden!(format!("{:?}", res));
-    }
-    for src in &srcs {
-        let prog = front::parser::program(src).unwrap();
-        let mut res = String::new();
-        lua::write_lua(&mut res, &prog).unwrap();
-        gilder::assert_golden!(res);
-    }
-
-    let srcs = [
-        r#"let a: num = "a""#,
-        r#"let a: {1, 2} = {1, 3}"#,
-        r#"let a: {1, 2} = {1}"#,
-        r#"let a: {1, 2} = {1, 2, 3}"#,
-        r#"let a: {1, [num]: num} = {1, 2, 3}"#,
-        r#"let a: {1, [num]: str} = {1, 2, 3}"#,
-        r#"let a: {1, [str]: num} = {1, 2, 3}"#,
-        r#"let f: (num, num) -> () = fn(a: num) -> () {}"#,
-    ];
-    for src in &srcs {
-        let prog = front::parser::program(src).unwrap();
-        let res = type_check::check_program(default_bindings(), &prog);
-        gilder::assert_golden!(format!("{:?}", res));
-    }
 }
---
+++ src/tests.rs
@@ -0,0 +1,77 @@
+use crate::{default_bindings, front::parser, lua::write_lua, type_check::check_program};
+
+#[test]
+fn test() {
+    let srcs = [
+        r#"
+fn main(a: num) -> num {
+    let b: num = 1
+    return a * 2 + b
+}
+"#,
+        r#"
+fn main() {
+    let b: num = 1 + 2
+}
+"#,
+        r#"
+let a: {1, 2} = {1, 2}
+"#,
+        r#"
+let a: num | str = 1 || "a" && 2
+"#,
+        r#"
+let a: {f: (num) -> num} = {
+    f: fn(a: num) -> num {
+        return a * 2
+    }
+}"#,
+        r#"
+fn f() -> () {
+    f()
+}"#,
+        r#"
+fn f() {
+    len!{1, 2, 3}
+}"#,
+        r#"
+let t: {[table]: num, [fn]: str} = {}
+t[{1, 2}] = 3
+t[fn() -> () {}] = "a"
+"#,
+        r#"
+{1: "1", [2]: "2", ["3"]: "3"}
+"#,
+        r#"
+let f: (num) -> () = fn(a: num, b: num) -> () {}
+let f: (num, num | ()) -> () = fn(a: num) -> () {}
+"#,
+    ];
+    for src in &srcs {
+        let prog = parser::program(src).unwrap();
+        let res = check_program(default_bindings(), &prog);
+        gilder::assert_golden!(format!("{:?}", res));
+    }
+    for src in &srcs {
+        let prog = parser::program(src).unwrap();
+        let mut res = String::new();
+        write_lua(&mut res, &prog).unwrap();
+        gilder::assert_golden!(res);
+    }
+
+    let srcs = [
+        r#"let a: num = "a""#,
+        r#"let a: {1, 2} = {1, 3}"#,
+        r#"let a: {1, 2} = {1}"#,
+        r#"let a: {1, 2} = {1, 2, 3}"#,
+        r#"let a: {1, [num]: num} = {1, 2, 3}"#,
+        r#"let a: {1, [num]: str} = {1, 2, 3}"#,
+        r#"let a: {1, [str]: num} = {1, 2, 3}"#,
+        r#"let f: (num, num) -> () = fn(a: num) -> () {}"#,
+    ];
+    for src in &srcs {
+        let prog = parser::program(src).unwrap();
+        let res = check_program(default_bindings(), &prog);
+        gilder::assert_golden!(format!("{:?}", res));
+    }
+}
